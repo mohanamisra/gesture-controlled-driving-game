a57afd455c4f4a59c0d8136e6450939e9f840154
Mohana Misra <iammohanamisra@gmail.com>
2024-11-30 14:42:01 +0530

    Code restructure

--- gesture_recogniser.py
+++ gesture_recogniser.py
@@ -18,42 +18,6 @@ def get_keypoints():
     np_sign = np.array(sign).flatten()
     print(np_sign.shape)
 
-
-mp_drawing = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hands
-
-cap = cv.VideoCapture(0)
-
-if not cap.isOpened():
-    print("We completely support your privacy requirements, but need to access your webcam for this app to work.")
-    exit()
-
-with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5, max_num_hands=1) as hands:
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            print("Exiting stream...")
-            break
-        img = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-        img.flags.writeable = False
-        results = hands.process(img)
-
-        img.flags.writeable = True
-
-        if results.multi_hand_landmarks:
-            for num, hand in enumerate(results.multi_hand_landmarks):
-                mp_drawing.draw_landmarks(img, hand, mp_hands.HAND_CONNECTIONS,
-                                          mp_drawing.DrawingSpec(color=(0, 0, 200), thickness=1, circle_radius=1),
-                                          mp_drawing.DrawingSpec(color=(0, 0, 200), thickness=2, circle_radius=2))
-
-        img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
-        cv.imshow('Webcam', img)
-        if cv.waitKey(1) & 0xFF == ord('q'):
-            break
-
-
-get_keypoints()
-
 path = os.path.join('data')
 actions = np.array(['go', 'stop', 'left', 'right', 'offence'])
 no_of_videos = 20
@@ -65,6 +29,45 @@ for action in actions:
             os.makedirs(os.path.join(path, action, str(video)))
         except:
             pass
+
+
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+cap = cv.VideoCapture(0)
+
+if not cap.isOpened():
+    print("We completely support your privacy requirements, but need to access your webcam for this app to work.")
+    exit()
+
+with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5, max_num_hands=1) as hands:
+    for action in actions:
+        for video in range(no_of_videos):
+            for frame in range(video_frame_len):
+                ret, frame = cap.read()
+                if not ret:
+                    print("Exiting stream...")
+                    break
+                img = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+                img.flags.writeable = False
+                results = hands.process(img)
+
+                img.flags.writeable = True
+
+                if results.multi_hand_landmarks:
+                    for num, hand in enumerate(results.multi_hand_landmarks):
+                        mp_drawing.draw_landmarks(img, hand, mp_hands.HAND_CONNECTIONS,
+                                                  mp_drawing.DrawingSpec(color=(0, 0, 200), thickness=1, circle_radius=1),
+                                                  mp_drawing.DrawingSpec(color=(0, 0, 200), thickness=2, circle_radius=2))
+
+                img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+                cv.imshow('Webcam', img)
+                if cv.waitKey(1) & 0xFF == ord('q'):
+                    break
+
+
+get_keypoints()
+
 
 cap.release()
 cv.destroyAllWindows()
